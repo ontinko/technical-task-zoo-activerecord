994999587e6d41834c6af79f699ca6003a7eb236
Anton Makarov <ontko@mailfence.com>
2023-11-10 15:18:05 +0200

    Added squirrels

--- animal.rb
+++ animal.rb
@@ -5,9 +5,12 @@ require 'active_record'
 class Animal < ActiveRecord::Base
   scope :males, -> { where(sex: 'male') }
   scope :females, -> { where(sex: 'female') }
+
   scope :age_less_than, ->(max_age) { where('age < ?', max_age) }
   scope :age_greater_than, ->(min_age) { where('age > ?', min_age) }
+
   scope :monkeys, -> { where(species: 'monkey') }
   scope :parrots, -> { where(species: 'parrot') }
   scope :bears, -> { where(species: 'bear') }
+  scope :squirrels, -> { where(species: 'squirrel') }
 end
