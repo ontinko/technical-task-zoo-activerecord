9b86d48df7fd15776a26dd15ef85548703929261
Anton Makarov <ontko@mailfence.com>
2023-11-10 15:14:22 +0200

    Added scopes

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 # gem "rails"
 
-gem "activerecord", "~> 7.1"
+gem 'activerecord', '~> 7.1'
 
-gem "pry", "~> 0.14.2"
+gem 'pry', '~> 0.14.2'
--- animal.rb
+++ animal.rb
@@ -3,19 +3,11 @@
 require 'active_record'
 
 class Animal < ActiveRecord::Base
-  def self.males
-    where(sex: 'male')
-  end
-
-  def self.females
-    where(sex: 'female')
-  end
-
-  def self.monkeys
-    where(species: 'monkey')
-  end
-
-  def self.parrots
-    where(species: 'parrot')
-  end
+  scope :males, -> { where(sex: 'male') }
+  scope :females, -> { where(sex: 'female') }
+  scope :age_less_than, ->(max_age) { where('age < ?', max_age) }
+  scope :age_greater_than, ->(min_age) { where('age > ?', min_age) }
+  scope :monkeys, -> { where(species: 'monkey') }
+  scope :parrots, -> { where(species: 'parrot') }
+  scope :bears, -> { where(species: 'bear') }
 end
